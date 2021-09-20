52ba65572458499fc1d5f3b9bd3a1711ee132a9a
Saeed Ashoor <saeedashoor.dev@gmail.com>
2021-09-20 22:40:05 +0100

    Pushing out the second commit

--- js/app.js
+++ js/app.js
@@ -9,11 +9,11 @@ let emailModal = document.getElementsByClassName("email-modal")[0];
 
 let closeModal = document.getElementsByClassName("email-modal__close-btn")[0];
 
-let decline = document.getElementsByClassName("email-modal__decline-offer") [0];
+let decline = document.getElementsByClassName("email-modal__decline-offer")[0];
 
 decline.addEventListener("click", () => {
-    emailModal.classList.remove("email-modal--visible");
-  });
+  emailModal.classList.remove("email-modal--visible");
+});
 
 closeModal.addEventListener("click", () => {
   emailModal.classList.remove("email-modal--visible");
