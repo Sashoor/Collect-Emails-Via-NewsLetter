eabd58292f2d1af5e0d30c92e27be8b479ab41d2
Saeed Ashoor <saeedashoor.dev@gmail.com>
2021-09-22 00:02:02 +0100

    Added email address validation REGEX, and also added error functionality

--- js/app.js
+++ js/app.js
@@ -5,11 +5,39 @@
 //   console.log(window.document);
 // };
 
+let emailState = false;
+
 let emailModal = document.getElementsByClassName("email-modal")[0];
 
 let closeModal = document.getElementsByClassName("email-modal__close-btn")[0];
 
 let decline = document.getElementsByClassName("email-modal__decline-offer")[0];
+
+let emailInput = document.getElementsByClassName("email-modal__input")[0];
+
+let emailButton = document.getElementsByClassName("email-modal__button")[0];
+
+function emailsValid(email) {
+  return /\S+@\S+\.\S+/.test(email);
+}
+
+emailButton.addEventListener("click", () => {
+  if (emailsValid(emailInput.value)) {
+    console.log(emailInput.value);
+  } else {
+    document
+      .getElementsByClassName("email-modal__form-group")[0]
+      .classList.add("email-modal__form-group--error");
+    document
+      .getElementsByClassName("email-modal__error-message")[0]
+      .classList.add("email-modal__error-message--active");
+  }
+});
+
+let showModal = () => {
+  if (emailState == false) emailModal.classList.add("email-modal--visible");
+  emailState = true;
+};
 
 decline.addEventListener("click", () => {
   emailModal.classList.remove("email-modal--visible");
@@ -19,7 +47,6 @@ closeModal.addEventListener("click", () => {
   emailModal.classList.remove("email-modal--visible");
 });
 
-document.addEventListener("mouseleave", () => {
-  emailModal.classList.add("email-modal--visible");
-  console.log("mouse left");
+document.body.addEventListener("mouseleave", () => {
+  showModal();
 });
