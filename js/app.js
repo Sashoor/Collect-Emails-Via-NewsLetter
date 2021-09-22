4b422af8efa7e8e58f3ba911da6e9e971ee52c9d
Saeed Ashoor <saeedashoor.dev@gmail.com>
2021-09-23 00:04:23 +0100

    Added Error functionaltiy, I also have added Thank you message to appear once news letter is sent with auto closure 3second timeset

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ let emailState = false;
 
 let emailModal = document.getElementsByClassName("email-modal")[0];
 
-let closeModal = document.getElementsByClassName("email-modal__close-btn")[0];
+let closeButton = document.getElementsByClassName("email-modal__close-btn")[0];
 
 let decline = document.getElementsByClassName("email-modal__decline-offer")[0];
 
@@ -17,33 +17,59 @@ let emailInput = document.getElementsByClassName("email-modal__input")[0];
 
 let emailButton = document.getElementsByClassName("email-modal__button")[0];
 
+let thankContainer = document.getElementsByClassName("email-thank")[0];
+
 function emailsValid(email) {
   return /\S+@\S+\.\S+/.test(email);
 }
-
-emailButton.addEventListener("click", () => {
-  if (emailsValid(emailInput.value)) {
-    console.log(emailInput.value);
-  } else {
-    document
-      .getElementsByClassName("email-modal__form-group")[0]
-      .classList.add("email-modal__form-group--error");
-    document
-      .getElementsByClassName("email-modal__error-message")[0]
-      .classList.add("email-modal__error-message--active");
-  }
-});
 
 let showModal = () => {
   if (emailState == false) emailModal.classList.add("email-modal--visible");
   emailState = true;
 };
 
+let addErrors = () => {
+  document
+    .getElementsByClassName("email-modal__form-group")[0]
+    .classList.add("email-modal__form-group--error");
+  document
+    .getElementsByClassName("email-modal__error-message")[0]
+    .classList.add("email-modal__error-message--active");
+};
+
+let removeErrors = () => {
+  document
+    .getElementsByClassName("email-modal__form-group")[0]
+    .classList.remove("email-modal__form-group--error");
+  document
+    .getElementsByClassName("email-modal__error-message")[0]
+    .classList.remove("email-modal__error-message--active");
+};
+
+let showThankMessage = () => {
+  thankContainer.classList.add("email-thank--success");
+  setTimeout(() => {
+    closeButton();
+  }, 3000);
+};
+
+emailInput.addEventListener("click", () => {
+  removeErrors();
+});
+
+emailButton.addEventListener("click", () => {
+  if (emailsValid(emailInput.value)) {
+    showThankMessage();
+  } else {
+    addErrors();
+  }
+});
+
 decline.addEventListener("click", () => {
   emailModal.classList.remove("email-modal--visible");
 });
 
-closeModal.addEventListener("click", () => {
+closeButton.addEventListener("click", () => {
   emailModal.classList.remove("email-modal--visible");
 });
 
